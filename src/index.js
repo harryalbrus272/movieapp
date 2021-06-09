fa8a786974c8defcf2759ad17bd1cf59e44f7c66
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-09 21:33:58 +0530

    add redux to the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 import reportWebVitals from './reportWebVitals';
-
+const store = createStore(movies);
+console.log(store);
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
