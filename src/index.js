281314755e007f875501574d2e78c7e520f9ffc2
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-11 09:21:52 +0530

    initialised and checked working of the middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import './index.css';
-import App from './components/App';
-import rootReducer from './reducers';
-import reportWebVitals from './reportWebVitals';
-const store = createStore(rootReducer);
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import "./index.css";
+import App from "./components/App";
+import rootReducer from "./reducers";
+import reportWebVitals from "./reportWebVitals";
+//Curried form of function logger
+//logger(obj)(next)(action)
+const logger = function ({ dispatch, getState }) {
+  return function (next){
+    return function (action) {
+      //middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
+};
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // //Dispatcher in place to dispatch the actions
 // store.dispatch({
@@ -13,12 +24,12 @@ const store = createStore(rootReducer);
 //   movies: []
 // });
 
-console.log('After State', store.getState());
+console.log("After State", store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App store={store} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
